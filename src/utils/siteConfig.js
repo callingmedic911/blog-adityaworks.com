48eac7fdab8ef5ae84ca6adb8ee84a3a5d8699ec
callingmedic911 <aditya@autospace.co>
2020-08-23 01:47:24 +0530

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Aditya's Blog`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Randomly logged pages`, // This allows an alternative site description for meta data for pages.
+    // siteTitleMeta: `Aditya's Blog`, // This allows an alternative site title for meta data for pages.
+    // siteDescriptionMeta: `Randomly logged pages`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
